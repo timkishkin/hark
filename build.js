c028ffdc610d49e3bc649436b0461c72ed954a66
Tim Kishkin <tkishkin@linkando.com>
2024-05-18 15:16:28 +0200

    Added ability to append comment to the bundle

--- build.js
+++ build.js
@@ -1,12 +1,37 @@
 var browserify = require('browserify'),
-    fs = require('fs');
+    fs = require('fs'),
+    through = require('through2');
 
+const headerComment =
+`/**
+ * hark.js
+ * Tiny browser/commonJS module that listens to an audio stream, and emits events indicating whether the user is speaking or not
+ * Modified by Linkando team
+ * Repo: https://github.com/timkishkin/hark
+ * Original repo: https://github.com/otalk/hark
+ * @story User Story 7456: AI copilot: real-time filling out of AC: PoC pt.1
+ */\n`;
+    
 var bundle = browserify();
 bundle.add('./hark');
 bundle.bundle({standalone: 'hark'})
+      .pipe(prependComment(headerComment))
       .pipe(fs.createWriteStream('hark.bundle.js'));
-
 
 var demo = browserify(['./example/demo.js'])
             .bundle()
             .pipe(fs.createWriteStream('./example/demo.bundle.js'));
+
+// Create a transform stream to prepend a comment
+function prependComment(comment) {
+    let isFirstChunk = true;
+    return through(function(chunk, enc, callback) {
+        if (isFirstChunk) {
+            isFirstChunk = false;
+            this.push(Buffer.from(comment + '\n'));
+        }
+        this.push(chunk);
+        callback();
+    });
+}
+
--- example/demo.bundle.js
+++ example/demo.bundle.js
@@ -126,7 +126,7 @@ for (var i = 0; i < 100; i++) {
   window.requestAnimationFrame(draw);
 })();
 
-},{"../hark.js":2,"attachmediastream":5,"bows":3,"getusermedia":4}],4:[function(require,module,exports){
+},{"../hark.js":2,"attachmediastream":5,"bows":4,"getusermedia":3}],3:[function(require,module,exports){
 // getUserMedia helper by @HenrikJoreteg
 var func = (navigator.getUserMedia ||
             navigator.webkitGetUserMedia ||
@@ -504,7 +504,7 @@ WildEmitter.mixin = function (constructor) {
 
 WildEmitter.mixin(WildEmitter);
 
-},{}],3:[function(require,module,exports){
+},{}],4:[function(require,module,exports){
 (function(window) {
   var logger = require('andlog'),
       goldenRatio = 0.618033988749895,
